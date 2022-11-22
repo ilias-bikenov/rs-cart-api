da55329507108c527b9013c9501373d43327ead4
ilias-bikenov <ilias.bikenov@gmail.com>
2022-11-22 11:40:03 +0600

    task-8

--- src/cart/services/cart.service.ts
+++ src/cart/services/cart.service.ts
@@ -3,29 +3,77 @@ import { Injectable } from '@nestjs/common';
 import { v4 } from 'uuid';
 
 import { Cart } from '../models';
+import { Client } from 'pg';
 
 @Injectable()
 export class CartService {
   private userCarts: Record<string, Cart> = {};
 
-  findByUserId(userId: string): Cart {
-    return this.userCarts[ userId ];
+  async findByUserId(userId: string): Promise<Cart> {
+    const client = new Client({
+      user: process.env.RDS_USER,
+      host: process.env.RDS_HOST,
+      database: process.env.RDS_DB,
+      password: process.env.RDS_PASSWORD,
+      port: parseInt(process.env.RDS_PORT),
+    });
+    console.log('find cart');
+    try {
+      await client.connect();
+      const {
+        rows: [cart],
+      } = await client.query(`SELECT id FROM carts 
+     LIMIT 1`);
+      const { rows: cartItems } = await client.query(
+        `SELECT products.id as id, 
+     products.title as title,
+     products.description as description,
+     products.price as price,
+     cart_items.count as count 
+     FROM cart_items
+    LEFT JOIN products ON products.id = cart_items.product_id WHERE cart_id = $1`,
+        [cart.id],
+      );
+      const items = cartItems.map(cartItem => {
+        return {
+          count: cartItem.count,
+          product: { ...cartItem, count: undefined },
+        };
+      });
+      console.log('return statement', { id: cart.id, items });
+      await client.end();
+
+      return { id: cart.id, items };
+    } catch (error) {
+      console.log(error);
+      await client.end();
+      return null;
+    }
   }
 
-  createByUserId(userId: string) {
-    const id = v4(v4());
-    const userCart = {
-      id,
-      items: [],
-    };
+  async createByUserId(userId: string) {
+    console.log('creating cart');
+    const client = new Client({
+      user: process.env.RDS_USER,
+      host: process.env.RDS_HOST,
+      database: process.env.RDS_DB,
+      password: process.env.RDS_PASSWORD,
+      port: parseInt(process.env.RDS_PORT),
+    });
+    await client.connect();
+    const {
+      rows: [cart],
+    } = await client.query(`INSERT INTO carts DEFAULT VALUES`);
+    await client.end();
 
-    this.userCarts[ userId ] = userCart;
-
-    return userCart;
+    return cart;
   }
 
-  findOrCreateByUserId(userId: string): Cart {
-    const userCart = this.findByUserId(userId);
+  async findOrCreateByUserId(userId: string): Promise<Cart> {
+    console.log('before req ');
+
+    const userCart = await this.findByUserId(userId);
+    console.log('after req');
 
     if (userCart) {
       return userCart;
@@ -34,22 +82,48 @@ export class CartService {
     return this.createByUserId(userId);
   }
 
-  updateByUserId(userId: string, { items }: Cart): Cart {
-    const { id, ...rest } = this.findOrCreateByUserId(userId);
+  async updateByUserId(userId: string, items: any) {
+    const { id, ...rest } = await this.findOrCreateByUserId(userId);
+    console.log(
+      { id, ...rest }.items.map(item =>
+        item.product.id === items.product.id
+          ? (item.count = items.count)
+          : item,
+      ),
+    );
 
+    const client = new Client({
+      user: process.env.RDS_USER,
+      host: process.env.RDS_HOST,
+      database: process.env.RDS_DB,
+      password: process.env.RDS_PASSWORD,
+      port: parseInt(process.env.RDS_PORT),
+    });
+    try {
+      await client.connect();
+      await client.query(
+        `UPDATE cart_items SET count = $1 WHERE product_id = $2`,
+        [items.count, items.product.id],
+      );
+      await client.end();
+    } catch (error) {
+      console.log(error);
+    }
     const updatedCart = {
       id,
-      ...rest,
-      items: [ ...items ],
-    }
+      items: rest.items.map(item =>
+        item.product.id === items.product.id
+          ? (item.count = items.count)
+          : item,
+      ),
+    };
 
-    this.userCarts[ userId ] = { ...updatedCart };
+    console.log(updatedCart);
 
-    return { ...updatedCart };
+    return { updatedCart };
   }
 
   removeByUserId(userId): void {
-    this.userCarts[ userId ] = null;
+    this.userCarts[userId] = null;
   }
-
 }
